1ad7f9454c3f34b92e4dcd4416a6f4efaa89519e
Duta <wheen.b@gmail.com>
2014-10-04 02:24:19 +0100

    Some tokens

--- Token.hs
+++ Token.hs
@@ -1,3 +1,15 @@
 module Token where
 
 data Token
+  = T_Word String
+  | T_Int  Int
+  | T_LCurly
+  | T_RCurly
+  | T_LParen
+  | T_RParen
+  | T_LessThan
+  | T_GreaterThan
+  | T_Colon
+  | T_Semicolon
+  | T_Equal
+    deriving (Eq, Show)

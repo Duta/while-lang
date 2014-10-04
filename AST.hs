94ef0e9e299d5b72acf6aaeaa7097c0b830ee0d7
Duta <wheen.b@gmail.com>
2014-10-04 03:19:46 +0100

    Some reduction

--- AST.hs
+++ AST.hs
@@ -34,7 +34,7 @@ data F_BOp
 
 data MiniAST
   = M_Seq [MiniAST]
-  | M_IfElse MiniExpr MiniExpr MiniExpr
+  | M_IfElse MiniExpr MiniAST MiniAST
   | M_While MiniExpr MiniAST
   | M_Assign Identifier MiniExpr
     deriving (Eq, Show)
@@ -61,4 +61,38 @@ data M_BOp
     deriving (Eq, Show)
 
 reduce :: FullAST -> MiniAST
-reduce = error "AST reduction not implemented"
+reduce ast = case ast of
+  F_Seq asts ->
+    M_Seq $ map reduce asts
+  F_If cond ast ->
+    reduce $ F_IfElse cond ast (F_Seq [])
+  F_IfElse cond ast1 ast2 ->
+    M_IfElse (reduceExpr cond) (reduce ast1) (reduce ast2)
+  F_While cond ast ->
+    M_While (reduceExpr cond) (reduce ast)
+  F_Assign var expr ->
+    M_Assign var (reduceExpr expr)
+
+reduceExpr :: FullExpr -> MiniExpr
+reduceExpr expr = case expr of
+  F_UOp op expr -> (case op of
+    F_Not -> M_UOp M_Not expr'
+    F_Neg -> M_UOp M_Neg expr')
+    where
+      expr' = reduceExpr expr
+  F_BOp op expr1 expr2 -> (case op of
+    F_Add -> M_BOp M_Add expr1' expr2'
+    F_Sub -> M_BOp M_Sub expr1' expr2'
+    F_Mul -> M_BOp M_Mul expr1' expr2'
+    F_Div -> M_BOp M_Div expr1' expr2'
+    F_Eq  -> M_BOp M_Eq  expr1' expr2'
+    F_Neq -> M_UOp M_Not (M_BOp M_Eq expr1' expr2'))
+    where
+      expr1' = reduceExpr expr1
+      expr2' = reduceExpr expr2
+  F_Var var ->
+    M_Var var
+  F_Int n ->
+    M_Int n
+  F_Bool b ->
+    M_Bool b
